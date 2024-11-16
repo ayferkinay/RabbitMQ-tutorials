8b80a7b36a3a4f708e8902d3a6129e7c8c9abbf1
ayferkinay <ayferkinay36@gmail.com>
2024-11-16 23:15:44 +0300

    Create a consumer as basic level

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//bağlantı oluştruma 
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new("amqp://guest:guest@localhost:5672/");
+
+
+
+//bağlantı aktifleştirme ve kanal açma 
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue'dan mesaj okuma
+EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    //kuyruktaki mesajın byte verisini getirip string halini gösterecek
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+};
+
+
+Console.Read();
